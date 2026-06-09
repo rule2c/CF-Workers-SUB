87bf1cded0930c0c6f1ac2e5da470e8ccb68fbce
rule2c <rule2c@gmail.com>
2026-06-09 09:43:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ https://cfxr.eu.org/getSub
 
 let urls = [];
 let subConverter = "suc.29161319.xyz"; //在线订阅转换后端：SUBAPI.cmliussss.net。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/rule2c/Customized_Rules/refs/heads/main/Clash_noleak.ini"; //订阅配置文件
+let subConfig = "https://raw.githubusercontent.com/rule2c/Customized_Rules/refs/heads/main/Clash_share.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
